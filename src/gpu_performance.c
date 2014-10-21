a54bdab4200d831424d863772473d6d54c617ed7
Guillermo Julián <gjulianm@outlook.com>
2014-10-21 16:02:14 +0200

    Improved performance analysis

--- src/gpu_performance.c
+++ src/gpu_performance.c
@@ -6,7 +6,7 @@
 
 #define NUMTESTS 40
 
-static void _measure_performance(StableDist *cpu_dist, StableDist *gpu_dist, double x)
+static void _measure_performance(StableDist *cpu_dist, StableDist *gpu_dist, double x, double alfa, double beta)
 {
     int i;
     double cpu_pdf = 0, gpu_pdf = 0;
@@ -26,32 +26,33 @@ static void _measure_performance(StableDist *cpu_dist, StableDist *gpu_dist, dou
     gpu_duration = (gpu_end - gpu_start) / NUMTESTS;
     cpu_duration = (cpu_end - cpu_start) / NUMTESTS;
 
-    printf("%3.5f\t%3.5f\n", cpu_duration, gpu_duration);
+    printf("%.2f\t%.2f\t%3.5f\t%3.5f\n", alfa, beta, cpu_duration, gpu_duration);
 }
 
 int main (void)
 {
     double alfas[] = { 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75 };
-    double betas[] = { 0.0, 0.5, 1.0 };
+    double betas[] = { 0.0, 0.25, 0.5, 0.75, 1.0 };
     double ev_points[] = { 1, 0, -1, 10, 1000, -1000 };
     double sigma = 1.0, mu = 0.0;
     StableDist *cpu_dist, *gpu_dist;
     int ai, bi, evi;
 
     printf("=== GPU/CPU performance tests for libstable ===\n");
+    stable_set_relTOL(1.2e-14);
 
     for (ai = 0; ai < sizeof alfas / sizeof(double); ai++)
     {
         for (bi = 0; bi < sizeof betas / sizeof(double); bi++)
         {   
-            printf("[α: %.2f\t β: %.2f\t]\n", alfas[ai], betas[bi]);
-
             cpu_dist = stable_create(alfas[ai], betas[bi], sigma, mu, 0);
             gpu_dist = stable_create(alfas[ai], betas[bi], sigma, mu, 0);
+
+
             stable_activate_gpu(gpu_dist);
 
             for (evi = 0; evi < sizeof ev_points / sizeof(double); evi++)
-                _measure_performance(cpu_dist, gpu_dist, ev_points[evi]);
+                _measure_performance(cpu_dist, gpu_dist, ev_points[evi], alfas[ai], betas[bi]);
 
             stable_free(cpu_dist);
             stable_free(gpu_dist);
