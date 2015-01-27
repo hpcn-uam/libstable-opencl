83fd3be4e13482c032f3b4a0d830356214b0dfe5
Guillermo Julián <gjulianm@outlook.com>
2015-01-27 23:57:43 +0100

    comment out uninteresting tests

--- src/opencl_tests.c
+++ src/opencl_tests.c
@@ -119,13 +119,13 @@ int main(int argc, char const *argv[])
 	srand(time(NULL));
 
     // test_kernel("opencl/perftests.cl", "array_sum_loop");
-    test_kernel("opencl/perftests.cl", "array_sum_reduction");
+    // test_kernel("opencl/perftests.cl", "array_sum_reduction");
     test_kernel("opencl/perftests.cl", "array_sum_twostage_loop");
     test_kernel("opencl/perftests.cl", "array_sum_twostage_loop_lc");
     test_kernel("opencl/perftests.cl", "array_sum_twostage_reduction");
     test_kernel("opencl/perftests.cl", "array_sum_twostage_half_wgs");
-    test_kernel("opencl/perftests.cl", "array_sum_twostage_two_wgs");
-    test_kernel("opencl/perftests.cl", "array_sum_2stage_lc");
+    // test_kernel("opencl/perftests.cl", "array_sum_twostage_two_wgs");
+    // test_kernel("opencl/perftests.cl", "array_sum_2stage_lc");
 
 	return 0;
 }
