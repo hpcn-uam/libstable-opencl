77c7dce7cab6b34ac553b1a0ee9c28b74771d467
Guillermo Julián <gjulianm@outlook.com>
2015-01-27 20:43:42 +0100

    More and more interesting data

--- src/opencl_tests.c
+++ src/opencl_tests.c
@@ -75,7 +75,7 @@ short test_kernel(const char* file, const char* kernel_name)
 	FILE* profile_f;
 	size_t workgroup_sizes[] = { 64, 128, 256, 512 };
 	size_t array_size;
-	size_t array_size_tests = 23;
+	size_t array_size_tests = 25;
     double bw;
     size_t array_bytes;
 	int wg_i, as_i;
