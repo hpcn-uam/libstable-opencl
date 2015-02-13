21c8309cacac3f0ddf68897f145329da567f58a0
Guillermo Julián <gjulianm@outlook.com>
2015-02-13 20:56:27 +0100

    Missing arguments

--- srclib/libstable/opencl_integ.c
+++ srclib/libstable/opencl_integ.c
@@ -129,6 +129,14 @@ short stable_clinteg_points(struct stable_clinteg *cli, double *x, double *pdf_r
     cli->h_args->k1 = dist->k1;
     cli->h_args->alfa = dist->alfa;
     cli->h_args->alfainvalfa1 = dist->alfainvalfa1;
+    cli->h_args->THETA_TH = stable_get_THETA_TH();
+    cli->h_args->theta0 = dist->theta0;
+    cli->h_args->xi = dist->xi;
+    cli->h_args->mu_0 = dist->mu_0;
+    cli->h_args->sigma = dist->sigma;
+    cli->h_args->xxi_th = stable_get_XXI_TH();
+    cli->h_args->c2_part = dist->c2_part;
+    cli->h_args->S = dist->S;
 
     if (dist->ZONE == GPU_TEST_INTEGRAND)
         cli->h_args->integrand = GPU_TEST_INTEGRAND;
