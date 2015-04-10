43cb631dd8aa36c599c85afdfae875fb27a87fa1
Guillermo Julián <gjulianm@outlook.com>
2015-04-10 19:51:35 +0200

    Initial implementation for the grid fitter

---
+++ includes/stable_gridfit.h
@@ -0,0 +1,34 @@
+#ifndef STABLE_GRIDFIT_H
+#define STABLE_GRIDFIT_H
+
+#include "stable_api.h"
+#include "opencl_integ.h"
+
+#define GRIDFIT_TEST_PER_DIM 4
+#define MAX_STABLE_PARAMS 4
+#define MAX_ITERATIONS 200 // Tip: Use the iterations_calc script in scripts folder
+#define WANTED_PRECISION 1
+
+struct stable_gridfit {
+	StableDist* initial_dist;
+	StableDist** fitter_dists;
+	size_t fitter_dist_count;
+	size_t fitter_dimensions;
+	size_t fitter_per_dimension[MAX_STABLE_PARAMS];
+	unsigned int current_iteration;
+	const double *data;
+	size_t data_length;
+	double corners[MAX_STABLE_PARAMS];
+	double centers[MAX_STABLE_PARAMS];
+	double point_sep[MAX_STABLE_PARAMS];
+	double contracting_coefs[MAX_STABLE_PARAMS];
+	cl_event* waiting_events;
+	double* likelihoods;
+	double max_likelihood;
+	double min_likelihood;
+	size_t min_fitter;
+};
+
+int stable_fit_grid(StableDist *dist, const double *data, const unsigned int length);
+
+#endif
---
+++ srclib/libstable/stable_gridfit.c
@@ -0,0 +1,174 @@
+#include "stable_gridfit.h"
+
+#define DIM_ALPHA 0
+#define DIM_BETA 1
+#define DIM_MU 2
+#define DIM_SIGMA 3
+
+static double initial_point_separation[] = { 0.1, 0.1, 0.1, 0.1 };
+static double initial_contracting_coefs[] = { 0.8, 0.8, 0.8, 0.8 };
+
+static void get_params_from_dist(StableDist* dist, double params[4])
+{
+	params[DIM_ALPHA] = dist->alfa;
+	params[DIM_BETA] = dist->beta;
+	params[DIM_MU] = dist->mu_0;
+	params[DIM_SIGMA] = dist->sigma;
+}
+
+static short set_params_to_dist(StableDist* dist, double* params, size_t params_count)
+{
+	double dist_current_params[MAX_STABLE_PARAMS];
+
+	get_params_from_dist(dist, dist_current_params);
+
+	for(size_t dim = 0; dim < params_count; dim++)
+		dist_current_params[dim] = params[dim];
+
+
+	return stable_setparams(dist, dist_current_params[DIM_ALPHA], dist_current_params[DIM_BETA],
+		dist_current_params[DIM_MU], dist_current_params[DIM_SIGMA], 0) == NOVALID;
+}
+
+static void calculate_upperleft_corner_point(struct stable_gridfit* gridfit)
+{
+	double dst_to_border;
+
+	for(size_t dim = 0; dim < gridfit->fitter_dimensions; dim++)
+	{
+		dst_to_border = gridfit->point_sep[dim] * ((double) gridfit->fitter_per_dimension[dim] - 1) / 2;
+		gridfit->corners[dim] = gridfit->centers[dim] - dst_to_border;
+	}
+}
+
+static short prepare_grid_params_for_fitter(struct stable_gridfit* gridfit, size_t fitter)
+{
+	size_t grid_coordinate;
+	double params[gridfit->fitter_dimensions];
+	size_t previous_dim_acc_size = 1;
+
+	previous_dim_acc_size = 1;
+
+	for(size_t dim = 0; dim < gridfit->fitter_dimensions; dim++)
+	{
+		grid_coordinate = (fitter / previous_dim_acc_size) % gridfit->fitter_per_dimension[dim];
+		previous_dim_acc_size *= gridfit->fitter_per_dimension[dim];
+		params[dim] = gridfit->corners[dim] + grid_coordinate * gridfit->point_sep[dim];
+	}
+
+	return set_params_to_dist(gridfit->fitter_dists[fitter], params, gridfit->fitter_dimensions);
+}
+
+static void point_sep_iterate(struct stable_gridfit* gridfit)
+{
+	for(size_t dim = 0; dim < gridfit->fitter_dimensions; dim++)
+		gridfit->point_sep[dim] *= gridfit->contracting_coefs[dim];
+}
+
+static void gridfit_init(struct stable_gridfit* gridfit, StableDist *dist, const double *data, const unsigned int length)
+{
+	gridfit->data = data;
+	gridfit->data_length = length;
+	gridfit->fitter_dimensions = MAX_STABLE_PARAMS;
+	gridfit->fitter_dist_count = 1;
+	gridfit->current_iteration = 0;
+
+	for(size_t i = 0; i < gridfit->fitter_dimensions; i++)
+	{
+		gridfit->fitter_per_dimension[i] = GRIDFIT_TEST_PER_DIM; // Same size for every dimension, for now;
+		gridfit->fitter_dist_count *= gridfit->fitter_per_dimension[i];
+	}
+
+	gridfit->initial_dist = dist;
+	gridfit->fitter_dists = calloc(gridfit->fitter_dist_count, sizeof(StableDist*));
+	gridfit->waiting_events = calloc(gridfit->fitter_dist_count, sizeof(cl_event));
+	gridfit->likelihoods = calloc(gridfit->fitter_dist_count, sizeof(double));
+
+	for(size_t i = 0; i < gridfit->fitter_dist_count; i++)
+	{
+		gridfit->fitter_dists[i] = stable_create(1, 0.5, 1, 1, 0);
+		stable_activate_gpu(gridfit->fitter_dists[i]);
+	}
+
+	memcpy(gridfit->point_sep, initial_point_separation, gridfit->fitter_dimensions * sizeof(double));
+	memcpy(gridfit->contracting_coefs, initial_contracting_coefs, gridfit->fitter_dimensions * sizeof(double));
+}
+
+static void set_new_center(struct stable_gridfit* gridfit, double* params)
+{
+	memcpy(gridfit->centers, params, gridfit->fitter_dimensions);
+}
+
+static void gridfit_iterate(struct stable_gridfit* gridfit)
+{
+	double pdf[gridfit->data_length];
+	StableDist* dist;
+
+	gridfit->max_likelihood = DBL_MIN;
+	gridfit->min_likelihood = DBL_MAX;
+
+	for(size_t i = 0; i < gridfit->fitter_dist_count; i++)
+	{
+		if(prepare_grid_params_for_fitter(gridfit, i) == 0)
+		{
+			dist = gridfit->fitter_dists[i];
+			stable_clinteg_points_async(&dist->cli, (double*) gridfit->data, gridfit->data_length, dist, gridfit->waiting_events + i);
+		}
+		else
+		{
+			gridfit->likelihoods[i] = DBL_MAX;
+			gridfit->waiting_events[i] = NULL;
+		}
+	}
+
+	for(size_t i = 0; i < gridfit->fitter_dist_count; i++)
+	{
+		dist = gridfit->fitter_dists[i];
+
+		if(gridfit->waiting_events[i])
+			stable_clinteg_points_end(&dist->cli, pdf, NULL, gridfit->data_length, dist, gridfit->waiting_events + i);
+		else
+			continue;
+
+		gridfit->likelihoods[i] = 0;
+
+		for(size_t point = 0; point < gridfit->data_length; point++)
+			gridfit->likelihoods[i] += -log(pdf[point]);
+
+		if(gridfit->likelihoods[i] > gridfit->max_likelihood)
+			gridfit->max_likelihood = gridfit->likelihoods[i];
+
+		if(gridfit->likelihoods[i] < gridfit->min_likelihood)
+		{
+			gridfit->min_likelihood = gridfit->likelihoods[i];
+			gridfit->min_fitter = i;
+		}
+	}
+}
+
+int stable_fit_grid(StableDist *dist, const double *data, const unsigned int length)
+{
+	struct stable_gridfit gridfit;
+	double likelihood_diff = DBL_MAX;
+	double best_params[MAX_STABLE_PARAMS];
+
+	gridfit_init(&gridfit, dist, data, length);
+	get_params_from_dist(dist, gridfit.centers);
+
+	while(gridfit.current_iteration < MAX_ITERATIONS && likelihood_diff > WANTED_PRECISION)
+	{
+		calculate_upperleft_corner_point(&gridfit);
+		gridfit_iterate(&gridfit);
+
+		get_params_from_dist(gridfit.fitter_dists[gridfit.min_fitter], best_params);
+		set_new_center(&gridfit, best_params);
+
+		point_sep_iterate(&gridfit);
+
+		likelihood_diff = gridfit.max_likelihood - gridfit.min_likelihood;
+	}
+
+	set_params_to_dist(dist, best_params, gridfit.fitter_dimensions);
+
+	return 0;
+}
