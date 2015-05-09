4fc565e6756e73094f77a7cc69fbff924906bcff
Guillermo Julián <gjulianm@outlook.com>
2015-05-09 16:48:58 +0200

    Removing commentary

--- src/fit_eval.c
+++ src/fit_eval.c
@@ -1,30 +1,3 @@
-/* tests/fittest
- *
- * Example program that test Libstable parameter estimation methods.
- *
- * Copyright (C) 2013. Javier Royuela del Val
- *                     Federico Simmross Wattenberg
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; If not, see <http://www.gnu.org/licenses/>.
- *
- *
- *  Javier Royuela del Val.
- *  E.T.S.I. Telecomunicación
- *  Universidad de Valladolid
- *  Paseo de Belén 15, 47002 Valladolid, Spain.
- *  jroyval@lpi.tel.uva.es
- */
 #include "stable_api.h"
 #include "benchmarking.h"
 #include "stable_gridfit.h"
