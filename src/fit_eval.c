c90bd27f5479b79257d34e4321b2b6620641704a
Guillermo Julián <gjulianm@outlook.com>
2015-05-10 22:09:12 +0200

    Detailed sample of the parameter space in fit_eval

--- src/fit_eval.c
+++ src/fit_eval.c
@@ -52,12 +52,12 @@ struct fitresult
 	variable ## _init += dist->variable; \
 } while(0)
 
-#define ALFA_START 0.3
+#define ALFA_START 0.1
 #define ALFA_END 1.9
-#define ALPHA_INCR 0.2
-#define BETA_START 0.55
+#define ALPHA_INCR 0.05
+#define BETA_START -0.9
 #define BETA_END 0.9
-#define BETA_INCR 0.2
+#define BETA_INCR 0.05
 #define MU_START -10
 #define MU_END 10
 #define MU_INCR 1
