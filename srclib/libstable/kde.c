94a77a3b47a115b33816f710c79a94658d49dacb
Guillermo Julián <guillermo.julian@estudiante.uam.es>
2016-06-13 12:50:50 +0200

    Formatting on kde.c

--- srclib/libstable/kde.c
+++ srclib/libstable/kde.c
@@ -5,14 +5,14 @@
  * Estimates the kernel density p(x) at a given value x from
  * an array of sample points.  Uses the default algorithm from
  * the R langauge's 'density' function.  Requires the GSL statistics
- * library.  
- *   
+ * library.
+ *
  * @section LICENCE
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -24,7 +24,7 @@
 
 #include "kde.h"
 
-/** Estimate bandwidth using Silverman's "rule of thumb" 
+/** Estimate bandwidth using Silverman's "rule of thumb"
  * (Silverman 1986, pg 48 eq 3.31).  This is the default
  * bandwith estimator for the R 'density' function.  */
 double nrd0(double x[], const int N)
@@ -32,17 +32,17 @@ double nrd0(double x[], const int N)
 	gsl_sort(x, 1, N);
 	double hi = gsl_stats_sd(x, 1, N);
 	double iqr =
-		gsl_stats_quantile_from_sorted_data (x,1, N,0.75) - 
-        gsl_stats_quantile_from_sorted_data (x,1, N,0.25);
-	double lo = GSL_MIN(hi, iqr/1.34);
-	double bw = 0.9 * lo * pow(N,-0.2);
-	return(bw);
+		gsl_stats_quantile_from_sorted_data(x, 1, N, 0.75) -
+		gsl_stats_quantile_from_sorted_data(x, 1, N, 0.25);
+	double lo = GSL_MIN(hi, iqr / 1.34);
+	double bw = 0.9 * lo * pow(N, -0.2);
+	return bw;
 }
 
 /* kernels for kernel density estimates */
 double gauss_kernel(double x)
-{ 
-	return exp(-(gsl_pow_2(x)/2))/(M_SQRT2*sqrt(M_PI)); 
+{
+	return exp(-(gsl_pow_2(x) / 2)) / (M_SQRT2 * sqrt(M_PI));
 }
 
 double kerneldensity(double *samples, double obs, size_t n)
@@ -50,10 +50,11 @@ double kerneldensity(double *samples, double obs, size_t n)
 	size_t i;
 	double h = GSL_MAX(nrd0(samples, n), 1e-6);
 	double prob = 0;
-	for(i=0; i < n; i++)
-	{
-		prob += gauss_kernel( (samples[i] - obs)/h)/(n*h);
-	}
+	printf("%lf\n", h);
+
+	for (i = 0; i < n; i++)
+		prob += gauss_kernel((samples[i] - obs) / h) / (n * h);
+
 	return prob;
 }
 
