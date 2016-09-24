3166460be3aa09efa3bd026b8a18a5d90cbc3c4a
Guillermo Julián <guillermo.julian@estudiante.uam.es>
2016-09-24 17:18:37 +0200

    Move initial estimator to a separate file

---
+++ includes/stable_mixture_initialestim.h
@@ -0,0 +1,19 @@
+/**
+ * Header file for the interface of the mixture initial estimator.
+ */
+
+#ifndef STABLE_MIXTURE_INITIALESTIM
+#define STABLE_MIXTURE_INITIALESTIM
+
+#include "stable_api.h"
+
+/**
+ * Given a set of data, prepare an initial estimation of the components of the mixture.
+ * @param dist   StableDist structure.
+ * @param data   Data to estimate.
+ * @param length Length of the data array.
+ */
+void stable_mixture_prepare_initial_estimation(
+	StableDist* dist, const double* data, const unsigned int length);
+
+#endif
