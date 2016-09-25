6bc2db10d989255622226546c7e43aa7071511a0
Guillermo Julián <guillermo.julian@estudiante.uam.es>
2016-09-25 14:19:54 +0200

    Add improved peak detection in two passes with smoother/noisier EPDF

--- srclib/libstable/stable_mixture_initialestim.c
+++ srclib/libstable/stable_mixture_initialestim.c
@@ -193,6 +193,21 @@ size_t _find_local_minmax(double* epdf, size_t* maxs, size_t* mins, size_t epdf_
 	return max_idx;
 }
 
+struct component_partition {
+	size_t begin_idx;
+	size_t end_idx;
+	size_t max_idx;
+};
+
+static int _compar_partition(const void* a, const void* b)
+{
+	const struct component_partition* pa = (const struct component_partition*) a;
+	const struct component_partition* pb = (const struct component_partition*) b;
+
+	// Cast to int to allow signed results
+	return ((int) pa->begin_idx) - ((int) pb->begin_idx);
+}
+
 void stable_mixture_prepare_initial_estimation(StableDist* dist, const double* data, const unsigned int length)
 {
 	size_t epdf_points = 5000;
@@ -200,13 +215,17 @@ void stable_mixture_prepare_initial_estimation(StableDist* dist, const double* d
 	double epdf_x[epdf_points];
 	double epdf[epdf_points], epdf_finer[epdf_points];
 	double epdf_start, epdf_end;
-	size_t maxs[epdf_points], mins[epdf_points], valid_max[epdf_points], valid_min[epdf_points];
+	double min_peak_dst;
+	size_t maxs[epdf_points], mins[epdf_points];
 	size_t maxs_finer[epdf_points], mins_finer[epdf_points];
-	size_t total_max, total_max_finer, valid_max_idx, max_idx, max_finer_idx;
+	size_t total_max, total_max_finer, max_idx, max_finer_idx;
+	struct component_partition mixture_partition[dist->max_mixture_components];
+	size_t current_partition, total_partitions;
 	size_t i;
 	double max_value;
 	size_t current_lowest_min_pos;
 	size_t extra_components = 0;
+	size_t extra_partitions = 0;
 	double mu_values[dist->max_mixture_components];
 	double sigma_values[dist->max_mixture_components];
 	StableDist* comp;
@@ -224,42 +243,115 @@ void stable_mixture_prepare_initial_estimation(StableDist* dist, const double* d
 	epdf_start = gsl_stats_quantile_from_sorted_data(samples, 1, length, 0.02);
 	epdf_end = gsl_stats_quantile_from_sorted_data(samples, 1, length, 0.98);
 
+	min_peak_dst = 0.05 * (epdf_end - epdf_start);
+
 	printf("Study range is [%lf, %lf], %zu points with step %lf\n", epdf_start, epdf_end, epdf_points, (epdf_end - epdf_end) / epdf_points);
 
 	calculate_epdf(samples, length, epdf_start, epdf_end, epdf_points, MIXTURE_KERNEL_ADJUST, epdf_x, epdf);
+	calculate_epdf(samples, length, epdf_start, epdf_end, epdf_points, 0.2 * MIXTURE_KERNEL_ADJUST, epdf_x, epdf_finer);
+
 	total_max = _find_local_minmax(epdf, maxs, mins, epdf_points, &max_value, epdf_x);
+	total_max_finer = _find_local_minmax(epdf_finer, maxs_finer, mins_finer, epdf_points, NULL, epdf_x);
+
 	current_lowest_min_pos = mins[0];
 
 	// The initial search of maximums is complete.
-	// Now let's filter and get only those big enough
-	for (valid_max_idx = 0, max_idx = 0, max_finer_idx = 0; max_idx < total_max; max_idx++) {
+	// Now let's filter and get only those big enough in the smoother EPDF
+	// This allows us to get a good picture of where are the maximums of the major
+	// mixture components without having to worry much about noise
+	for (current_partition = 0, max_idx = 0; max_idx < total_max; max_idx++) {
 		if (epdf[current_lowest_min_pos] > epdf[mins[max_idx]])
 			current_lowest_min_pos = mins[max_idx];
 
 		if (epdf[maxs[max_idx]] > 0.05 * max_value) {
-			valid_max[valid_max_idx] = maxs[max_idx];
-			valid_min[valid_max_idx] = current_lowest_min_pos;
+			mixture_partition[current_partition].begin_idx = current_lowest_min_pos;
+			mixture_partition[current_partition].max_idx = maxs[max_idx];
+
+			if (current_partition > 0)
+				mixture_partition[current_partition - 1].end_idx = current_lowest_min_pos;
+
 			current_lowest_min_pos = maxs[max_idx]; // Find minimum from here.
-			valid_max_idx++;
-			printf("Max %zu at %lf = %lf is valid (valid max idx %zu)\n",
-				   max_idx, epdf_x[maxs[max_idx]], epdf[maxs[max_idx]], valid_max_idx);
+			printf("Max %zu at %lf = %lf is valid (assigned to component %zu)\n",
+				   max_idx, epdf_x[maxs[max_idx]], epdf[maxs[max_idx]], current_partition);
+
+			current_partition++;
 		} else {
 			extra_components++;
 			printf("Max %zu discarded (%.3lf %% of max)\n", max_idx, 100 * epdf[maxs[max_idx]] / max_value);
 		}
 	}
 
-	valid_min[valid_max_idx] = mins[max_idx]; // Add the last minimum (there must be n max, n + 1 mins).
+	mixture_partition[current_partition - 1].end_idx = mins[max_idx]; // Add the last minimum (there must be n max, n + 1 mins).
+	total_partitions = current_partition;
 
-	total_max = valid_max_idx;
+	// Now a second search on the set of local min/max obtained from the finer
+	// EPDF estimation. This one has much more noise, but it does not smooth out
+	// peaks that could signal the existence of another mixture. In this case,
+	// we will only select peaks differing significantly from their value in the
+	// smoother EPDF.
+	for (max_idx = 0; max_idx < total_max_finer; max_idx++) {
+		size_t max_pos = maxs_finer[max_idx];
+		double difference_ratio = epdf_finer[max_pos] / epdf[max_pos];
 
-	stable_set_mixture_components(dist, total_max);
+		if (difference_ratio > 1.4 && epdf[max_pos] > 0.05 * max_value) {
+			// Found a peak that grows considerably when compared to the smoother EPDF.
+			// Most probably, this is a peak of a mixture, so we have to insert a new partition.
+
+			// Search for the first partition which has a maximum with X value higher than the one
+			// we're treating now
+			size_t part_idx = 0;
+			size_t minval_idx;
+			size_t part_max;
+
+			while (part_idx < total_partitions && epdf_x[mixture_partition[part_idx].begin_idx] < epdf_x[max_pos])
+				part_idx++;
+
+			printf("Found extra peak ratio %lf at x[%zu] = %lf, comp after is %zu\n", difference_ratio, max_pos, epdf_x[max_pos], part_idx);
+
+			if (part_idx > 0) {
+				// Modify the partition before this one
+				part_max = mixture_partition[part_idx - 1].max_idx;
+
+				minval_idx = gsl_stats_min_index(epdf_finer + part_max, 1, max_pos - part_max) + part_max;
+				mixture_partition[total_partitions].begin_idx = minval_idx;
+				mixture_partition[part_idx - 1].end_idx = minval_idx;
+			} else {
+				// No partition before this one, the begin index of the new partition is the begin index
+				// of the one after.
+				mixture_partition[total_partitions].begin_idx = mixture_partition[part_idx].begin_idx;
+			}
+
+			if (part_idx < total_partitions) {
+				// Modify the partition after this one
+				part_max = mixture_partition[part_idx].max_idx;
+
+				minval_idx = gsl_stats_min_index(epdf_finer + max_pos, 1, part_max - max_pos) + max_pos;
+				mixture_partition[total_partitions].end_idx = minval_idx;
+				mixture_partition[part_idx].begin_idx = minval_idx;
+			} else {
+				// No partition after this one, the end index of the new partition is the end
+				// of the one before
+				mixture_partition[total_partitions].end_idx = mixture_partition[part_idx - 1].end_idx;
+			}
+
+			mixture_partition[total_partitions].max_idx = max_pos;
+			total_partitions++;
+			extra_partitions++;
+
+			// Re-sort the partitions to allow search of the next maximum.
+			// Use mergesort instead of qsort as it behaves better on almost-sorted data.
+			mergesort(mixture_partition, total_partitions, sizeof(struct component_partition), _compar_partition);
+		}
+	}
+
+	printf("Found %zu possible extra components, for a total of %zu\n", extra_partitions, total_partitions);
+	stable_set_mixture_components(dist, total_partitions);
 
 	for (i = 0; i < dist->num_mixture_components; i++) {
 		comp = dist->mixture_components[i];
 
-		size_t comp_begin = valid_min[i];
-		size_t comp_end = valid_min[i + 1];
+		size_t comp_begin = mixture_partition[i].begin_idx;
+		size_t comp_end = mixture_partition[i].end_idx;
 		double max_x = epdf_x[comp_end];
 		size_t sample_len = 0;
 
@@ -278,7 +370,6 @@ void stable_mixture_prepare_initial_estimation(StableDist* dist, const double* d
 		data_offset = sample_len;
 	}
 
-	printf("Found %zu possible extra components\n", extra_components);
 
 	// Configure the death/birth probabilities
 	printf("Configuring extra component probabilities\n");
