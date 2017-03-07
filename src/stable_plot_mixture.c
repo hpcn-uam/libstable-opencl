70d7a70c3b80fc4bdc64ed6d64f84853ac41aaef
Guillermo Julián <guillermo.julian@estudiante.uam.es>
2017-03-07 22:47:51 +0100

    Improve mixture progress monitor

--- src/stable_plot_mixture.c
+++ src/stable_plot_mixture.c
@@ -10,20 +10,28 @@ int main(int argc, char **argv)
 	size_t args_per_component = 5;
 	StableDist* dist;
 	size_t i;
-	double xmin = 1.4, xmax = 2.1;
-	size_t num_samples = 400;
-	double *x, pdf[num_samples], step;
+	double xmin, xmax;
+	double resolution = 0.001;
+	double *x, *pdf, step;
+	size_t extra_args = 2;
+	size_t num_samples;
 
 	dist = stable_create(1, 0, 1, 0, 0);
 
-	stable_set_mixture_components(dist, (argc - 1) / args_per_component);
+	stable_set_mixture_components(dist, (argc - 1 - extra_args) / args_per_component);
+
+	xmin = strtod(argv[1], NULL);
+	xmax = strtod(argv[2], NULL);
+
+	num_samples = (xmax - xmin) / resolution;
+	pdf = calloc(num_samples, sizeof(double));
 
 	for (i = 0; i < dist->num_mixture_components; i++) {
-		dist->mixture_components[i]->alfa = strtod(argv[1 + i * args_per_component], NULL);
-		dist->mixture_components[i]->beta = strtod(argv[2 + i * args_per_component], NULL);
-		dist->mixture_components[i]->mu_0 = strtod(argv[3 + i * args_per_component], NULL);
-		dist->mixture_components[i]->sigma = strtod(argv[4 + i * args_per_component], NULL);
-		dist->mixture_weights[i] = strtod(argv[5 + i * args_per_component], NULL);
+		dist->mixture_components[i]->alfa = strtod(argv[1 + extra_args + i * args_per_component], NULL);
+		dist->mixture_components[i]->beta = strtod(argv[2 + extra_args + i * args_per_component], NULL);
+		dist->mixture_components[i]->mu_0 = strtod(argv[3 + extra_args + i * args_per_component], NULL);
+		dist->mixture_components[i]->sigma = strtod(argv[4 + extra_args + i * args_per_component], NULL);
+		dist->mixture_weights[i] = strtod(argv[5 + extra_args + i * args_per_component], NULL);
 	}
 
 	vector_npoints(&x, xmin, xmax, num_samples, &step);
